ef6e6fa333a5558d74ff76b032847e3e2655248e
mohanned-sameh <mohanned2678@gmail.com>
2024-05-09 23:58:36 +0300

    Refactor users table schema in 0x00-MySQL_Advanced/1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,12 +1,13 @@
---- creates a table users
---- id int not null auto increment and primary
---- email varchar(255) not null and unique
---- name varchar(255)
---- country encruments of countries US,CO, TN not null
---- default country is US
+-- Creates a table users with below attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- country, enum of countries: US, CO and TN, never null (default=US)
+-- If table exists, script will not fail, can be executed on any database
+
 CREATE TABLE IF NOT EXISTS users (
        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
        email VARCHAR(255) NOT NULL UNIQUE,
        name VARCHAR(255),
-       country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+       country ENUM ('US', 'CO', 'TN') NOT NULL
 );
