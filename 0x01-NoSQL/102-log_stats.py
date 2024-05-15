c9142a4ba66dcf9713837e4a150cb59a5bfb5bbf
mohanned-sameh <mohanned2678@gmail.com>
2024-05-16 00:05:45 +0300

    chore: Refactor log_stats function to improve readability and performance

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,32 +1,24 @@
 #!/usr/bin/env python3
-"""
-Log stats
-"""
+""" provides some stats about Nginx logs stored in MongoDB """
 from pymongo import MongoClient
 
 
-def log_stats():
-    """
-    provides some stats about Nginx logs stored in MongoDB
-    """
+def main():
+    """provides some stats about Nginx logs stored in MongoDB"""
     client = MongoClient("mongodb://127.0.0.1:27017")
-    logs = client.logs.nginx
-    total = logs.count_documents({})
-    get = logs.count_documents({"method": "GET"})
-    post = logs.count_documents({"method": "POST"})
-    put = logs.count_documents({"method": "PUT"})
-    patch = logs.count_documents({"method": "PATCH"})
-    delete = logs.count_documents({"method": "DELETE"})
-    path = logs.find({"method": "GET", "path": "/status"}).count()
-    print(f"{total} logs")
+    nginx_c = client.logs.nginx
+
+    n_logs = nginx_c.count_documents({})
+    print(f"{n_logs} logs")
+    methods: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
-    print(f"\tmethod GET: {get}")
-    print(f"\tmethod POST: {post}")
-    print(f"\tmethod PUT: {put}")
-    print(f"\tmethod PATCH: {patch}")
-    print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
+    for m in methods:
+        c: int = nginx_c.count_documents({"method": m})
+        print(f"\tmethod {m}: {c}")
+
+    st_c: int = nginx_c.count_documents({"method": "GET", "path": "/status"})
+    print(f"{st_c} status check")
 
 
 if __name__ == "__main__":
-    log_stats()
+    main()
