c08eb6cd1860676ece0e40fa036a842699b45f9f
Mohanned-sameh <Mohanned2678@gmail.com>
2024-05-16 13:13:35 +0300

    chore: Refactor get_page function to count the number of requests
    Refactor the get_page function to include a wrapper decorator that counts the number of requests made. This will provide valuable statistics for tracking and monitoring purposes. The decorator increments a counter in Redis for each request made and then calls the original get_page function. This change improves the functionality and maintainability of the codebase.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,37 +7,28 @@ from typing import Callable
 import requests
 import redis
 
+r = redis.Redis()
 
-def track_get_page(fn: Callable) -> Callable:
-    """Decorator for get_page"""
 
-    @wraps(fn)
-    def wrapper(url: str) -> str:
-        """Wrapper that:
-        - check whether a url's data is cached
-        - tracks how many times get_page is called
+def count_requests(method: Callable) -> Callable:
+    """
+    Count the number of requests
+    """
+
+    @wraps(method)
+    def wrapper(url):
         """
-        client = redis.Redis()
-        client.incr(f"count:{url}")
-        cached_page = client.get(f"{url}")
-        if cached_page:
-            return cached_page.decode("utf-8")
-        response = fn(url)
-        client.set(f"{url}", response, 10)
-        return response
+        Wrapper function
+        """
+        r.incr(f"count:{url}")
+        return method(url)
 
     return wrapper
 
 
-@track_get_page
+@count_requests
 def get_page(url: str) -> str:
     """
-    Args:
-
-        url: url to get
-    Returns:
-
-            the HTML content of the URL
+    Get the HTML content of a page
     """
-    response = requests.get(url)
-    return response.text
+    return requests.get(url).text
